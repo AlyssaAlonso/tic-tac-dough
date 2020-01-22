a21519b95c3306e99d282d6ad86cab2f40ad0078
Alyssa <Alyssa@Alyssas-MacBook.fios-router.home>
2020-01-22 17:01:17 -0500

    updated css

--- build/precache-manifest.736134dbf8825a3448fb36bef5d9508e.js
+++ build/precache-manifest.736134dbf8825a3448fb36bef5d9508e.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "215298fb73a03bbb18a2",
+    "revision": "32bc7707bb80d6428820",
     "url": "/tic-tac-dough/static/css/main.2a8bb191.chunk.css"
   },
   {
-    "revision": "215298fb73a03bbb18a2",
-    "url": "/tic-tac-dough/static/js/main.260a08dc.chunk.js"
+    "revision": "32bc7707bb80d6428820",
+    "url": "/tic-tac-dough/static/js/main.b4b23b12.chunk.js"
   },
   {
     "revision": "1a2701e74e51cead51ee",
@@ -16,15 +16,15 @@ self.__precacheManifest = [
     "url": "/tic-tac-dough/static/js/2.0e3f6a1a.chunk.js"
   },
   {
-    "revision": "d0735c65c55defe63effbd63c114f898",
-    "url": "/tic-tac-dough/static/media/O.d0735c65.png"
-  },
-  {
     "revision": "36bd3b60dc386052215152cee9eae342",
     "url": "/tic-tac-dough/static/media/X.36bd3b60.png"
   },
   {
-    "revision": "4f8c77b5cabdeec12d8f3ecdb29d79e2",
+    "revision": "d0735c65c55defe63effbd63c114f898",
+    "url": "/tic-tac-dough/static/media/O.d0735c65.png"
+  },
+  {
+    "revision": "61167c1dee6e4192a15ddfa8eaf2f076",
     "url": "/tic-tac-dough/index.html"
   }
 ];
